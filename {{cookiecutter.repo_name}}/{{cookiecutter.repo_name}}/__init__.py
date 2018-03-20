4a5b6e81d04cb254adcc75a16761a8041e031ed0
Daniel Smith <malorian@me.com>
2018-03-20 09:49:35 -0400

    Adds versioneer info to __init__

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -7,4 +7,12 @@
 # Safe to remove with Python 3-only code
 from __future__ import absolute_import
 
+# Add imports here
 from .{{cookiecutter.first_module_name}} import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
