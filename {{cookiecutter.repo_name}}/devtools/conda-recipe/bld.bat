8170d39718e47f3d410d070ef50d6d9fdf07dc3d
Levi Naden <levi.naden@choderalab.org>
2018-07-17 13:39:39 -0400

    use --no-deps correctly

--- {{cookiecutter.repo_name}}/devtools/conda-recipe/bld.bat
+++ {{cookiecutter.repo_name}}/devtools/conda-recipe/bld.bat
@@ -1,3 +1,3 @@
 # Build the python package, don't let setuptools/pip try to get packages
-"%PYTHON%" setup.py install --no-deps
+"%PYTHON%" setup.py develop --no-deps
 if errorlevel 1 exit 1
--- {{cookiecutter.repo_name}}/devtools/conda-recipe/build.sh
+++ {{cookiecutter.repo_name}}/devtools/conda-recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build the python package, don't let setuptools/pip try to get packages
-$PYTHON setup.py install --no-deps
+$PYTHON setup.py develop --no-deps
