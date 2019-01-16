e42ec64b7e5b28c1173c1961d31b3ae8ba915408
Daniel Smith <malorian@me.com>
2019-01-15 20:42:58 -0500

    Better validation of repo/module names, closes #61

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,13 +7,24 @@ If any error is raised, the cookie cutter creation fails and crashes
 import re
 import sys
 
-
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+EMAIL_REGEX = r'[^@]+@[^@]+\.[^@]+'
 
+repo_name = '{{ cookiecutter.repo_name }}'
 module_name = '{{ cookiecutter.first_module_name }}'
 
-if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: {} is not a valid Python module name!'.format(module_name))
+author_email = '{{ cookiecutter.author_email }}'
+
+for key in [repo_name, module_name]:
+    if not re.match(MODULE_REGEX, key):
+        print(key, re.match(MODULE_REGEX, key))
+        print('ERROR: "{}" is not a valid Python module name!'.format(key))
+
+        # exits with status 1 to indicate failure
+        sys.exit(1)
+
+if not re.match(EMAIL_REGEX, author_email):
+    print('ERROR: "{}" is not a valid email address!'.format(author_email))
 
     # exits with status 1 to indicate failure
     sys.exit(1)
