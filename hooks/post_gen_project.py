c8fd7d3da9bd5a6588093a62611c1ac92c44e516
Jessica Nash <janash@vt.edu>
2019-07-27 23:39:46 -0500

    modify post_gen script to init only if not already in repository

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -17,7 +17,7 @@ def decode_string(string):
         return string
 
 
-def invoke_shell(command):
+def invoke_shell(command, return_output=False):
     try:
         output = sp.check_output(command, shell=True, stderr=sp.STDOUT)
     except sp.CalledProcessError as e:
@@ -26,19 +26,31 @@ def invoke_shell(command):
         print(e.output)
         raise e
     print(decode_string(output))
+    if return_output:
+            return decode_string(output)
 
 
 def git_init_and_tag():
     """Invoke the initial git and tag with 0.0.0 to make an initial version for Versioneer to ID"""
-    # Initialize git
-    invoke_shell("git init")
-    # Add files
-    invoke_shell("git add .")
+    
+    # Check if we are in a git repository
+    directory_status = invoke_shell("git status", return_output=True)
+    # Create a repository if not already in one.
+    if 'fatal' in directory_status:
+        # Initialize git
+        invoke_shell("git init")
+
+    # Add files created by cookiecutter 
+    invoke_shell(F"git add .")
     invoke_shell(
         "git commit -m \"Initial commit after CMS Cookiecutter creation, version {}\"".format(
             '{{ cookiecutter._cms_cc_version }}'))
-    # Set the 0.0.0 tag
-    invoke_shell("git tag 0.0.0")
+    
+    # Check for a tag
+    version = invoke_shell("git tag", return_output=True)
+
+    if not version:
+        invoke_shell("git tag 0.0.0")
 
 
 def remove_windows_ci():
@@ -46,7 +58,6 @@ def remove_windows_ci():
     if include_windows == "n":
         # Remove with appveyor to be a safe delete
         os.remove("appveyor.yml")
-
 
 remove_windows_ci()
 git_init_and_tag()
