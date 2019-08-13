e140e911007cc91c1ff64006557526d17c5629a9
Daniel Smith <malorian@me.com>
2019-08-13 08:52:19 -0400

    Docs: fixes reponame template misses

--- {{cookiecutter.repo_name}}/docs/api.rst
+++ {{cookiecutter.repo_name}}/docs/api.rst
@@ -4,4 +4,4 @@ API Documentation
 .. autosummary::
    :toctree: autosummary
 
-   projectname.canvas
+   {{cookiecutter.repo_name}}.canvas
--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -17,7 +17,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import projectname
+import {{cookiecutter.repo_name}}
 
 
 # -- Project information -----------------------------------------------------
