bd54e7980758edcd0ec8637b7256504e010b76ae
Jaime RGP <jaime.rogue@gmail.com>
2020-02-11 14:58:08 +0100

    Put initialize_conda.sh under devtools/github-actions

--- {{cookiecutter.repo_name}}/devtools/github-actions/initialize_conda.sh
+++ {{cookiecutter.repo_name}}/devtools/github-actions/initialize_conda.sh
@@ -3,14 +3,6 @@
 case ${CI_OS} in
     windows*)
         eval "$(${CONDA}/condabin/conda.bat shell.bash hook)";;
-    macOS*)
-        eval "$(${CONDA}/condabin/conda shell.bash hook)";;
     *)
-        eval "$(conda shell.bash hook)";;
+        eval "$(${CONDA}/condabin/conda shell.bash hook)";;
 esac
-
-if [[ -d ${CONDA}/envs/test ]] || [[ -d ${HOME}/.conda/envs/test ]]; then
-    conda activate test
-else
-    conda activate
-fi
