1692ca0b086b0087d2d759505879e3358149e66b
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-05-23 20:20:46 +0200

    Migrate conda support to goanpeca's action

--- {{cookiecutter.repo_name}}/devtools/github-actions/initialize_conda.sh
+++
@@ -1,8 +0,0 @@
-# $CI_OS is $matrix.os, as exported in GHA *.yaml
-# $CONDA (miniconda installation path) is always defined in the GHA virtual environments
-case ${CI_OS} in
-    windows*)
-        eval "$(${CONDA}/condabin/conda.bat shell.bash hook)";;
-    *)
-        eval "$(${CONDA}/condabin/conda shell.bash hook)";;
-esac
