8104e5edf836d35cb3ed0c1cf300c01de4b7531a
radifar <m.radifar05@gmail.com>
2021-08-10 17:06:35 +0700

    fix import priority in test file

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -3,9 +3,11 @@ Unit and regression test for the {{cookiecutter.repo_name}} package.
 """
 
 # Import package, test suite, and other packages as needed
-import {{cookiecutter.repo_name}}
-import pytest
 import sys
+
+import pytest
+import {{cookiecutter.repo_name}}
+
 
 def test_{{cookiecutter.repo_name}}_imported():
     """Sample test, will always pass so long as import statement worked."""
