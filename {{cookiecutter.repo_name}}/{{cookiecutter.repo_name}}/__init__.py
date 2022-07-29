e1e1f4a97e2824456854af5628615931bba9933b
M. Eric Irrgang <ericirrgang@gmail.com>
2022-07-29 20:29:49 +0300

    Migrate from versioneer to versioningit.
    * Use a PEP-517-only build.
    * Remove setup.py since it is no longer necessary.
    
    Ref #149

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -3,9 +3,5 @@
 # Add imports here
 from .{{cookiecutter.first_module_name}} import *
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+
+from ._version import __version__
